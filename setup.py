9cd3b14eacb26511752032e0d05a0f0970b5ccf1
Gustavo Pessarelo Smulkowski Souza <gustavo.pessarelo@gmail.com>
2023-05-06 00:30:50 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analise de correlacao entre estatisticas em partidas de temporada regular da NBA',
+    author='Gustavo Pessarelo Smulkowski Souza',
+    license='MIT',
+)
